4eeacd428d4c33b8ce44399267a1d0c039046189
Nhat Tan <140130862+nhattannn222@users.noreply.github.com>
2025-12-08 09:15:04 +0700

    Delivery Push v5

--- backend/index.js
+++ backend/index.js
@@ -9,5 +9,5 @@ app.use('/api', routes);
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   // Đổi thành v3 để lát check log cho sướng
-  console.log(`Backend v4 running on port ${port}`);
+  console.log(`Backend v5 running on port ${port}`);
 });
